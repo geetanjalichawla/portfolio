5235bc6d3532cf83254becf62ca95396e9c3bdfb
geetanjalichawla <geetc1919@gmail.com>
2023-09-17 23:28:34 +0530

    fixing error for netlify

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,6 +1,5 @@
 
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 const Banner = () => {
     const toRotate =["Web Devloper ", "FullStack Devloper " ,"Student"];
